4e12d3193cfe0a4541e3b7b79f3b6ce582e8faaf
ldwfalcon <66138904+ldwfalcon@users.noreply.github.com>
2021-06-14 15:03:33 -0700

    Update testtiinnngg11.py

--- testtiinnngg11.py
+++ testtiinnngg11.py
@@ -17,21 +17,21 @@ with serial.Serial('/dev/ttyUSB0', 57600) as ser:
         rawMsgGreen =  msg2[1].strip("green: ")
         rawMsgYellow =  msg2[2].strip("yellow: ")
         if i = 1:
-            tolleranceQ = input("Do you want to change the tolerance of red, green or red.  Write the number you want it to be or keep them as Red:"+ tolRed +"Green:"+ tolGreen + "Yellow:"+ tolYellow + "or \ for no and seperate them with a coma.")
+            tolleranceQ = input("Do you want to change the tolerance of red, green or red.  Write the number you want it to be or keep them as Red:"+ tolRed +"Green:"+ tolGreen + "Yellow:"+ tolYellow + "or ] for no and seperate them with a coma.")
             splitTollAns = tolleranceQ.split(",")
-            if splitTollAns[0] != "\":
+            if splitTollAns[0] != "]":
                 tollRed = splitTollAns[0]
-            if splitTollAns[1] != "\":
+            if splitTollAns[1] != "]":
                 tollGreen = splitTollAns[1]
-            if splitTollAns[2] != "\":
+            if splitTollAns[2] != "]":
                 tollYellow = splitTollAns[2]
             caneWeightCalComm = input("Remove the cane from the ground and type \")
-            if caneWeightCalComm = "\":
+            if caneWeightCalComm = "[":
                 initLightMsgRed = rawMsgRed
                 initLightMsgGreen = rawMsgGreen
                 initLightMsgYellow = rawMsgYellow
             caneWeightCalComm = input("Place the cane on the ground and type \")
-            if caneWeightCalComm = "\":
+            if caneWeightCalComm = "]":
                 initSelfMsgRed = rawMsgRed
                 initSelfMsgGreen = rawMsgGreen
                 initSelfMsgYellow = rawMsgYellow
@@ -39,7 +39,11 @@ with serial.Serial('/dev/ttyUSB0', 57600) as ser:
         msgGreen = rawMsgGreen - initMsgGreen
         msgYellow = rawMsgYellow - initMsgYellow
         if initSelfMsgRed < initLightMsgRed - tollRed:
-                                      print("hello")
+                                      print("Red on ground")
+        if initSelfMsgGreen < initLightMsgGreen - tollGreen:
+                                      print("Green on ground")
+        if initSelfMsgYellow < initLightMsgYellow - tollYellow:
+                                      print("Yellow on ground")
         print(i,msgRed, msgGreen, msgYellow)
-        sleep(.5)
+        sleep(.01)
     quit()
