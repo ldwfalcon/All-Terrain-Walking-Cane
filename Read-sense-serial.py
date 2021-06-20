a2c68ebd34a74644744a4e3f51fdc41ec2899dcd
ldwfalcon <66138904+ldwfalcon@users.noreply.github.com>
2021-06-19 19:46:36 -0700

    Update Read-sense-serial.py

--- Read-sense-serial.py
+++ Read-sense-serial.py
@@ -1,6 +1,6 @@
 import serial
-def readSensors():
-  with serial.Serial('/dev/ttyUSB0', 57600) as ser:
+def readSensors(a):
+    with serial.Serial('/dev/ttyUSB0', 57600) as ser:
           x= ser.readline()
           l = x.rstrip()
           msgString = str(l)
@@ -8,24 +8,53 @@ def readSensors():
           msg = ''.join(character[1:100])
           msg2 =  (msg.strip("'")).split(",")
           redRawSensVal = msg2[0].strip("'red: ")
-          greenRawSensVal
-          yellowRawSensVal
+          greenRawSensVal = msg2[1].strip("green: ")
+          yellowRawSensVal = msg2[2].strip("yellow: ")
           #read red
           
-          if redSense != "error":
-              rawMsgRed =  float(redSens)#remove 'red: ' from string and co
+          if redRawSensVal != "error":
+              rawMsgRed =  float(redRawSensVal)#remove 'red: ' from string and co'
           else:
               print("Error with Red. ")
               
               #read green 
-          if msg2[1].strip("green: ") != "error":
-              rawMsgGreen =  float(msg2[1].strip("green: "))
+          if greenRawSensVal != "error":
+              rawMsgGreen =  float(greenRawSensVal)
           else:
               print("Error with Green. ")
               
               #read yellow 
-          if msg2[2].strip("yellow: ") != "error":
-              rawMsgYellow =  float(msg2[2].strip("yellow: "))
+          if yellowRawSensVal != "error":
+              rawMsgYellow =  float(yellowRawSensVal)
           else:
               print("Error with Yellow. ")
-readSensors()
+              
+
+          if a.count(',') == 0:
+              print('hi')
+              if a == 'red':
+                  print(rawMsgRed)
+              if a == 'green':
+                  print(rawMsgGreen)
+              if a == 'yellow':
+                  print(rawMsgYellow)
+          if a.count(',') == 1:
+              colorRequest = a.strip(', ')
+              print('hi1')
+              for i in range(2):
+                  if a[i] == 'red':
+                      print(rawMsgRed)
+                  if a[i] == 'green':
+                      print(rawMsgGreen)
+                  if a[i] == 'yellow':
+                      print(rawMsgYellow)
+          if a.count(',') == 2:
+              print('hi2')
+              colorRequest = a.strip(', ')
+              for i in range(3):
+                  if a[i] == 'red':
+                      print(rawMsgRed)
+                  if a[i] == 'green':
+                      print(rawMsgGreen)
+                  if a[i] == 'yellow':
+                      print(rawMsgYellow)
