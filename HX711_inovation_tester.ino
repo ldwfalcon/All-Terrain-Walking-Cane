821b5080f11fc9b86af80e7ae17f9b77c4ef4871
ldwfalcon <66138904+ldwfalcon@users.noreply.github.com>
2021-09-29 17:36:30 -0700

    Actual calibration used
    After a month of trying to smooth out and properly calibrate the sensor data in python, I realized that my code for the Arduino was not utilizing the HX711 library enough because it was not using the conversion and calibration it already offered. Know the calibration happens here and it outputs the data in grams

--- HX711_inovation_tester.ino
+++ HX711_inovation_tester.ino
@@ -14,50 +14,51 @@ int yellow_zero2 = 0;
 HX711 redscale;
 HX711 greenscale;
 HX711 yellowscale;
-int r =10000; //rounding number 
 
+float Rcalibration_factor = 2230;
+float Gcalibration_factor = 2230;
+float Ycalibration_factor = 2230;
+int r =1; //rounding number 
+int i = 1;
 void setup() {
   Serial.begin(57600);
   redscale.begin(LOADCELL_DOUT_PIN_RED, LOADCELL_SCK_PIN_RED);
   greenscale.begin(LOADCELL_DOUT_PIN_green, LOADCELL_SCK_PIN_green);
   yellowscale.begin(LOADCELL_DOUT_PIN_yellow, LOADCELL_SCK_PIN_yellow);
+  redscale.set_scale();
+  greenscale.set_scale();
+  yellowscale.set_scale();
+  redscale.tare();
+  greenscale.tare();
+  yellowscale.tare();
+  long Rzero_factor = redscale.read_average(); //Get a baseline reading
+  long Gzero_factor = greenscale.read_average(); //Get a baseline reading
+  long Yzero_factor = yellowscale.read_average(); //Get a baseline reading
 
 }
 
 void loop() {
-
-
-  if (Serial.read() == 't') {
-    delay(100);
-     red_zero2 = redscale.read()/r*r;
-     green_zero2 = greenscale.read()/r*r;
-     yellow_zero2 = yellowscale.read()/r*r;
-    Serial.println("taring");
-    delay(100);
-  }
-
+  
   if (redscale.is_ready()) {
-    int redreading = redscale.read()/r*r - red_zero2;
-    Serial.print("red: ");
-    Serial.print(redreading);
-  } else {
-    Serial.print("red: error");
-  }
-  if (greenscale.is_ready()) {
-    int greenreading = greenscale.read()/r*r - green_zero2;
-    Serial.print(", green: ");
-    Serial.print(greenreading );
-  } else {
-    Serial.print(",green: error");
-  }
-  if (yellowscale.is_ready()) {
-    int yellowreading = yellowscale.read()/r*r - yellow_zero2;
-    Serial.print(", yellow: ");
-    Serial.println(yellowreading);
+    int a = i++;
+    redscale.set_scale(Rcalibration_factor);
+    greenscale.set_scale(Gcalibration_factor);
+    yellowscale.set_scale(Ycalibration_factor);
+    //int redreading = (((round((redscale.read()/r)))*r - red_zero2));
+    //int greenreading = (((round((greenscale.read()/r)))*r - green_zero2)*1);
+    //int yellowreading = (((round((yellowscale.read()/r)))*r - yellow_zero2)*1);
+    float redreading = redscale.get_units();
+    float greenreading = greenscale.get_units();
+    float yellowreading = yellowscale.get_units();
+    //Serial.print(a);
+    //Serial.print(",");
+    Serial.print(redreading/2);
+    Serial.print(",");
+    Serial.print(greenreading/2);
+    Serial.print(",");
+    Serial.println(yellowreading/2);
 
-  } else {
-    Serial.println(",yellow: error");
   }
-  delay(75);
+  delay(111);
 
 }
